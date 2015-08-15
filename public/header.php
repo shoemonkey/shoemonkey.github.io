b350d252566015da4873fbc308466c145ff5a1c5
SupGames <SupGames/>
2015-08-15 15:46:22 -0700

    header add

---
+++ public/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sup Games</title>
+		<link href="../public/shoestrap/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="../public/shoestrap/css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="../public/shoestrap/styles.css" rel="stylesheet"/>
+        <link href="../css/style.css" rel="stylesheet"/>
+
+        <meta charset='utf-8'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	</head>
---
+++ templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sup Games</title>
+		<link href="../public/shoestrap/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="../public/shoestrap/css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="../public/shoestrap/styles.css" rel="stylesheet"/>
+        <link href="../css/style.css" rel="stylesheet"/>
